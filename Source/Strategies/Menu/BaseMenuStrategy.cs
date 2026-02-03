cb2afb86f6313b7bb8017f79221b393923788486
Natalia Krzus <nkrzus95@gmail.com>
2026-02-03 22:16:32 +0100

    Added Main & Base menu strategy

---
+++ Source/Strategies/Menu/BaseMenuStrategy.cs
@@ -0,0 +1,47 @@
+﻿using IO5Plan.Strategies.Menu.Interfaces;
+using IO5Plan.Strategies.Menu.Models;
+
+namespace IO5Plan.Strategies.Menu
+{
+    public abstract class BaseMenuStrategy : IMenu
+    {
+        public abstract IList<MenuItem> MenuItems { get; set; }
+        public abstract string MenuMessage { get; set; }
+        public void Show()
+        {
+            bool isOver = false;
+            while (!isOver)
+            {
+                showMainMenuDetails();
+                int choise = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+                var selectedMenuItem = MenuItems.FirstOrDefault(item => item.MenuNumber == choise);
+
+                if (selectedMenuItem != null)
+                {
+                    selectedMenuItem.Menu.Show();
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("Niepoprawny wybór");
+                    Console.WriteLine("");
+                }
+                if (choise==0)
+                {
+                    isOver = true;
+                }
+            }
+        }
+        private void showMainMenuDetails()
+        {
+            Console.WriteLine(MenuMessage);
+            foreach (var item in MenuItems)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.Write("Wybierz opcję: ");
+        }
+    }
+}
+
---
+++ Source/Strategies/Menu/MainMenuStrategy.cs
@@ -0,0 +1,22 @@
+﻿using IO5Plan.Strategies.Menu.Interfaces;
+using IO5Plan.Strategies.Menu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO5Plan.Strategies.Menu
+{
+    public class MainMenuStrategy:BaseMenuStrategy,IMenu
+    {
+        public override string MenuMessage { get; set; } = "MENU GŁÓWNE:";
+        public override IList<Models.MenuItem> MenuItems { get; set; } = [
+        new MenuItem {MenuMessage="Wprowadź dane wejściowe", MenuNumber=1,Menu=new ImportDataStrategy() },
+        new MenuItem {MenuMessage="Wygeneruj plany lekcji", MenuNumber=2,Menu=new GeneratePlansStrategy() },
+        new MenuItem {MenuMessage="Wyświetl wygenerowane plany", MenuNumber=3, Menu=new ShowGenetaredPlansStrategy()},
+        new MenuItem {MenuMessage="Zapisz wybrany plan do pliku", MenuNumber=4, Menu = new SaveSelectedPlanStrategy()},
+        new MenuItem {MenuMessage="Wyjście", MenuNumber=0, Menu= new ExitMenuStrategy() },
+        ];
+    }
+}
